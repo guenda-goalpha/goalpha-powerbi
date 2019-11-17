7d55add63695c8e9caad2e3e83ae22c540aa9068
Ogers <ogers.ruda@gmail.com>
2019-11-17 16:07:00 +0100

    Added UI which gets the query and endpoint for the API

---
+++ powerbi_ui/power_bi.php
@@ -0,0 +1,41 @@
+<?php
+//$servername = "localhost";
+//$username = "root";
+//$password = "";
+//$dbname = "datas";
+
+$servername = "35.240.25.3";
+$username = "postgres";
+$password = "imbEvent2019";
+$dbname = "goalpha";
+
+$connStr = "host=35.240.25.3 port=5432 dbname=goalpha user=postgres password=imbEvent2019";
+$conn = pg_connect($connStr);
+
+if (@$conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+if (isset($_POST['endpoint']) && isset($_POST['query']))
+{
+    $endpoint = $_POST['endpoint'];
+    $query = $_POST['query'];
+
+    //Heqim whitespaces of the string
+    $endpoint = trim($endpoint);
+    $query = trim($query);
+
+    //replace space with underscore
+    $endpoint = str_replace(" ","_",$endpoint);
+
+    $sql = "INSERT INTO datas (end_point, query) VALUES ('".$endpoint."','".$query."')";
+    if (pg_query($conn,$sql)) {
+        echo "Te dhenat u shtuan!";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+
+?>
